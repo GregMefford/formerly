a59eb411559ef84da96d4dda8f297c9c7b9b47c6
Greg Mefford <gregory.mefford@gmail.com>
2010-05-21 18:59:37 -0400

    Created first test (it should fail since it formerly does not exist yet.)

---
+++ spec/formerly_spec.rb
@@ -0,0 +1,11 @@
+# formerly_spec.rb
+require 'formerly'
+
+describe Formerly, "Best-Case text-scraping" do
+	it "knows how to expand tabs to spaces intelligently" do
+		input    = "This	is	some	text	that	has	4-space	tabs	in	it."
+		expected = "This    is  some    text    that    has 4-space tabs  in    it."
+		result = Formerly.tabs_to_spaces input
+		result.should = expected
+  end
+end
