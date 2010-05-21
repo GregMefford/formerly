4fe8361de5fed42366b206cf293efcaa9ced95e8
Greg Mefford <gregory.mefford@gmail.com>
2010-05-21 19:15:44 -0400

    Converted code's tabs to spaces and added a case.

--- formerly.rb
+++ formerly.rb
@@ -1,7 +1,7 @@
 #formerly.rb
 
 module Formerly
-	def self.tabs_to_spaces(line)
-		return "This    is  some    text    that    has 4-space tabs  in    it."
-	end
+    def self.tabs_to_spaces(line, tab_size=8)
+        return "This    is  some    text    that    has 4-space tabs  in    it."
+    end
 end
--- spec/formerly_spec.rb
+++ spec/formerly_spec.rb
@@ -2,10 +2,17 @@
 require 'formerly'
 
 describe Formerly, "Best-Case text-scraping" do
-	it "knows how to expand tabs to spaces intelligently" do
-		input    = "This	is	some	text	that	has	4-space	tabs	in	it."
-		expected = "This    is  some    text    that    has 4-space tabs  in    it."
-		result = Formerly.tabs_to_spaces input
-		result.should == expected
+  it "knows how to expand 4-space tabs to spaces intelligently" do
+    input    = "This\tis\tsome\ttext\tthat\thas\t4-space\ttabs\tin\tit."
+    expected = "This    is  some    text    that    has 4-space tabs  in    it."
+    result = Formerly.tabs_to_spaces input, 4
+    result.should == expected
+  end
+
+  it "knows how to expand 8-space tabs to spaces intelligently" do
+    input    = "This\tis\tsome\ttext\tthat\thas\t8-space\ttabs\tin\tit."
+    expected = "This        is      some    text    that    has     4-space tabs    in      it."
+    result = Formerly.tabs_to_spaces input, 8
+    result.should == expected
   end
 end
