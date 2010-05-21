eb9e82b99941d6e9e5e1c6910ca7ca0e1ba873ac
Greg Mefford <gregory.mefford@gmail.com>
2010-05-21 19:05:38 -0400

    Made the first test pass. Notice it was wrong...

---
+++ formerly.rb
@@ -0,0 +1,7 @@
+#formerly.rb
+
+module Formerly
+	def self.tabs_to_spaces(line)
+		return "This    is  some    text    that    has 4-space tabs  in    it."
+	end
+end
--- spec/formerly_spec.rb
+++ spec/formerly_spec.rb
@@ -6,6 +6,6 @@ describe Formerly, "Best-Case text-scraping" do
 		input    = "This	is	some	text	that	has	4-space	tabs	in	it."
 		expected = "This    is  some    text    that    has 4-space tabs  in    it."
 		result = Formerly.tabs_to_spaces input
-		result.should = expected
+		result.should == expected
   end
 end
