bb02ec675da46beba6d65f3dd1859ec2049fb4d0
Greg Mefford <gregory.mefford@gmail.com>
2010-05-21 20:03:36 -0400

    Imple-docu-mented a one-liner from the Internet.

--- formerly.rb
+++ formerly.rb
@@ -1,7 +1,19 @@
 #formerly.rb
 
 module Formerly
-    def self.tabs_to_spaces(line, tab_size=8)
-        return "This    is  some    text    that    has 4-space tabs  in    it."
+    def self.tabs_to_spaces(line, size=8)
+      # Tweaked based on code from http://www.bloovis.com/wordpress/?p=19
+      # What is does:
+      #   substitute all occurrances of:
+      #     (stuff other than a tab character),
+      #     followed by a tab character
+      #   with:
+      #     (That stuff we found),
+      #     followed by:
+      #       x spaces, where x is:
+      #         tab size - (length of stuff we found), modulo (tab size)
+      # Note: Since that asterisk is there around the [^\t],
+      #   the stuff before the tab can be nothing, so this works for \t\t.
+      return line.gsub(/([^\t]*)\t/) {$1 + " " * (size - $1.length % size) }
     end
 end
--- spec/formerly_spec.rb
+++ spec/formerly_spec.rb
@@ -2,16 +2,23 @@
 require 'formerly'
 
 describe Formerly, "Best-Case text-scraping" do
+  it "knows how to expand 2-space tabs to spaces intelligently" do
+    input    = "This\tis\tsome\ttext\tthat\thas\t\t2-space\ttabs\tin\tit."
+    expected = "This  is  some  text  that  has   2-space tabs  in  it."
+    result = Formerly.tabs_to_spaces input, 2
+    result.should == expected
+  end
+
   it "knows how to expand 4-space tabs to spaces intelligently" do
-    input    = "This\tis\tsome\ttext\tthat\thas\t4-space\ttabs\tin\tit."
-    expected = "This    is  some    text    that    has 4-space tabs  in    it."
+    input    = "This\tis\tsome\ttext\tthat\thas\t\t4-space\ttabs\tin\tit."
+    expected = "This    is  some    text    that    has     4-space tabs    in  it."
     result = Formerly.tabs_to_spaces input, 4
     result.should == expected
   end
 
   it "knows how to expand 8-space tabs to spaces intelligently" do
-    input    = "This\tis\tsome\ttext\tthat\thas\t8-space\ttabs\tin\tit."
-    expected = "This        is      some    text    that    has     4-space tabs    in      it."
+    input    = "This\tis\tsome\ttext\tthat\thas\t\t8-space\ttabs\tin\tit."
+    expected = "This    is      some    text    that    has             8-space tabs    in      it."
     result = Formerly.tabs_to_spaces input, 8
     result.should == expected
   end
